6b2ddba16896a70ad7839343aeeacb3fccbb28dc
Vera Chashchina <chaschinavera@gmail.com>
2024-03-30 19:11:07 +0300

    clean project

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/utils/getTime.tsx
+++
@@ -1,9 +0,0 @@
-export function getTime() {
-  const time = new Date();
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
-  if (minutes < 10)
-    return `${hours}:0${minutes}`
-  else
-    return `${hours}:${minutes}`;
-}
