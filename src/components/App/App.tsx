37634c27318fa6b195e387ac3e1855c9179a92a4
Vera Chashchina <chaschinavera@gmail.com>
2024-03-30 23:13:36 +0300

    add colors in tailwind

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-[375px] h-[100%] flex flex-col">
+
+    </div>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "human22": "url('/images/nashi-proekty/Human22.png')",
+      },
+      colors: {
+        white: "#fff",
+        black: "#000",
+        blue: { dark: "#1D2749", light: "#80D1FF" },
+
+        purple: {
+          text: "#8D59E9",
+          icon: "#8B79EE",
+          primary: "rgba(134, 56, 229, 0.80)",
+        },
+        orange: "#F8A51B",
+        violet: "#DA71FF",
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
